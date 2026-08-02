244a966d852b841c4ace8297bebb11c471423726
Ayush-Roy04 <royayush264@gmail.com>
2026-08-03 01:23:12 +0530

    Add files via upload

---
+++ CIT_DAY-2/Q2.py
@@ -0,0 +1,36 @@
+"""Create one variable of each data type:
+int
+float
+str
+bool
+list
+tuple
+dictionary
+set"""
+
+print("for int")
+a = 24
+print(type(a))
+print("for float")
+b = 3.14
+print(type(b))
+print("for str")
+c = "Hello World"
+print(type(c))
+print("for Bool")
+d = True
+print(type(d))
+print("for list")
+e = [1, 2, 3]
+print(type(e))
+print("for tuple")
+f = (4, 5, 6)
+print(type(f))
+print("for dict")
+g = {"name": "Ayush", "age": 21}
+print(type(g))
+print("for set")
+h = {7, 8, 9}
+print(type(h))
+
+
