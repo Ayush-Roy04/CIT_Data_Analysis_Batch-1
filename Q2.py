235267b9020e71145598aad46ffde2319b5f2827
Ayush-Roy04 <royayush264@gmail.com>
2026-08-03 01:28:45 +0530

    Delete Q2.py

--- Q2.py
+++
@@ -1,5 +0,0 @@
-#Function with Parameters Write a function that accepts a student name and print: Hello <student_name>, Welcome!
-
-def student(Student_name):
-    print(f"Hello {Student_name}, Welcome to Python Programming!")
-student("Ayush")
